761ef17aa1e6a16b81af835675e1d10c29324f77
nim <nimisha.k.vijay@gmail.com>
2020-07-12 23:47:28 +0530

    trying to fix snake body

--- script.js
+++ script.js
@@ -10,9 +10,13 @@
 const printf = args => console.log(args);
 
 
-const gridEl = document.getElementById('grid');
-const foodEl = document.getElementById('food').style;
-const snakeEl = document.getElementById('snake').style;
+const gridEl = document.getElementById('Grid');
+const foodEl = document.getElementById('Food').style;
+const snakeContainer = document.getElementById('Snake');
+snakeContainer.innerHTML = '<div id="snake0"></div>'
+const snakeEl = document.getElementById('snake0').style;
+snakeEl.position = 'absolute';
+
 const cellWidth = 21.33;	// in px
 const cellHeight = 21.33; // in px
 const rows = Math.floor(gridEl.clientWidth/cellWidth); // 16pt = 21.33 px
@@ -26,6 +30,11 @@ snakeEl.height = cellHeight + 'px';
 let [x, y] = [1, 1];
 let dir = 'r';
 let totalEaten = 0;
+let snakePos = [{  x: 1, y: 1 }];
+let foodPos = [{  
+	x: Math.floor(Math.random() * rows),
+	y: Math.floor(Math.random() * cols)
+}]
 
 const changeDir = (event) => {
 	switch(event.keyCode) {
@@ -38,11 +47,51 @@ const changeDir = (event) => {
 }
 document.addEventListener('keydown', (event) => (dir = changeDir(event)));
 
-
-const showSnake = (x, y) => {
-	snakeEl.left = (x*cellWidth) + 'px';
-	snakeEl.top = (y*cellHeight) + 'px';
+const showSnake = (snakePos) => {
+	for (let i = 0; i < snakePos.length; ++i) {
+		let tempSnake = document.getElementById('snake' + i).style;
+		tempSnake.left = (snakePos[i].x * cellWidth) + 'px';
+		tempSnake.top = (snakePos[i].y * cellHeight) + 'px';
+	}
 }
+
+const updateSnakePos = (newX, newY) => {
+	snakePos[0].x = newX;
+	snakePos[0].y = newY;
+	for (let i = 1; i < snakePos.length; ++i) {
+		snakePos[i].x = snakePos[i-1].x;
+		snakePos[i].y = snakePos[i-1].y;
+	}
+}
+
+const getNewFoodLocation = () => {
+	foodPos = {  
+		x: Math.floor(Math.random() * rows),
+		y: Math.floor(Math.random() * cols)
+	};
+	foodEl.left = foodPos.x * cellWidth + 'px';
+	foodEl.top = foodPos.y * cellHeight + 'px';
+}
+
+const checkEaten = () => {
+	return (snakePos[0].x == foodPos.x) && (snakePos[0].y == foodPos.y);
+}
+
+const addSnake = () => {
+	// update snakePos
+	++totalEaten;
+	printf(snakePos);
+	snakePos.push({
+		x: snakePos[snakePos.length - 1].x -1,
+		y: snakePos[snakePos.length - 1].y +1
+	})
+	printf(snakePos);
+	// update the DOM
+	snakeContainer.innerHTML += '<div id=snake' + totalEaten + '></div>';
+	showSnake(snakePos);
+	getNewFoodLocation();
+}
+
 
 const updateGrid = () => {
 	switch (dir) {
@@ -53,19 +102,17 @@ const updateGrid = () => {
 	}
 	x = x < 0 ? (x + rows)%rows : x%rows;
 	y = y < 0 ? (y + cols)%cols : y%cols;
-	showSnake(x, y);
-	if ((snakeEl.left == foodEl.left) && (snakeEl.top == foodEl.top)) {
-		getNewFoodLocation();
-	}
-	printf([[snakeEl.left, snakeEl.top], [foodEl.left, foodEl.top], dir]);
+	snakePos[0].x = x;
+	snakePos[0].y = y;
+
+	if (checkEaten()) addSnake(); 
+	updateSnakePos(x, y);
+	showSnake(snakePos);
+
 }
 
-const getNewFoodLocation = () => {
-	foodEl.left = Math.floor(Math.random() * rows) * cellWidth + 'px';
-	foodEl.top = Math.floor(Math.random() * cols) * cellHeight + 'px';
-}
 
 getNewFoodLocation();
-setInterval(updateGrid, 100);
+setInterval(updateGrid, 300);
 
 
